c47bf311e0458c50947ba25d5c6cded35d24671b
zhangmin <zm0700@163.com>
2022-07-31 10:48:57 +0800

    2022-7-31 1

---
+++ springSecurity/src/main/java/org/zm/spring/security/SpringSecurityApplication.java
@@ -0,0 +1,17 @@
+package org.zm.spring.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * SpringSecurity 启动类 *
+ * author Neo-HJ.Niu *
+ * time 2022/7/30
+ */
+@SpringBootApplication
+public class SpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityApplication.class, args);
+    }
+}
